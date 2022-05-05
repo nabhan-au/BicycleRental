a7a82a177e823cabd5fc681e32f2e9c4b2603f1b
Nabhan Suwanachote <nabhan.au@gmail.com>
2022-05-05 22:41:39 +0700

    branch for doc

--- main.py
+++
@@ -1,8 +0,0 @@
-from utilities.session_db import SessionDB
-
-db = SessionDB()
-bicycle = db.get_bicycle().get_bicycle_from_brand('3T')
-# users = db.get_bicycle().get_bicycle_from_type('Touring Bike')
-print(bicycle)
-
-user = db.get_users().get_user_from_age(30)
